4802af58b749363c3d567e31ee174f0731d3332f
Paulina Ramirez Cardona <pauramirezca@unal.edu.co>
2023-11-02 11:04:58 -0500

    Se crearon los modelos, los controladores y las migraciones del proyecto

---
+++ app/Http/Controllers/Historial_CompraController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Historial_CompraController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/TallaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TallaController extends Controller
+{
+    //
+}
